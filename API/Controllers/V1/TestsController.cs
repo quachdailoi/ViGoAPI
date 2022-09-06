9db3bd75db037f2c8a1e47f366a510b149925e87
quachdailoi <loiqd.work@gmail.com>
2022-09-06 20:51:24 +0700

    [LoiQD] Configure version for APIs in Swagger UI.

---
+++ API/Controllers/V1/TestsController.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Interfaces.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers.V1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [Authorize]
+    [ApiVersion("1.0")]
+    public class TestsController : BaseController<TestsController>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TestsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpDelete("user/{code}")]
+        
+        public async Task<IActionResult> DeleteUser(string code)
+        {
+            var user = await _unitOfWork.Users.GetUserByCode(code).Include(x => x.Accounts).Include(x => x.File).FirstOrDefaultAsync();
+
+            if (user == null) return NotFound("Not found user with code to delete.");
+
+            await _unitOfWork.Users.Remove(user);
+            foreach (var acc in user.Accounts)
+            {
+                await _unitOfWork.Accounts.Remove(acc);
+            }
+            if (user.File != null)
+            await _unitOfWork.Files.Remove(user.File);
+
+            return Ok("Delete user successfully.");
+        }
+    }
+}
