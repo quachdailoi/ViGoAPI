c19f0005510fdcde15f5bebca62d5de880fdc048
DESKTOP-4BHH352\Win <datdt.dev.work@gmail.com>
2022-11-01 00:38:24 +0700

    [Notification]: implement push notification via socket

---
+++ API/Models/DTO/NotificationDTO.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Shares.Enums;
+
+namespace API.Models.DTO
+{
+    public class NotificationDTO : BaseDTO
+    {
+        public Guid Code { get; set; } = Guid.NewGuid();
+        public string Description { get; set; } = string.Empty;
+        public Notifications.Types Type { get; set; }
+        public object? Data { get; set; } = null;
+        public Notifications.Status Status { get; set; } = Notifications.Status.Active;
+        public int? UserId { get; set; } = null;
+        public Events.Types EventId { get; set; }
+    }
+}
--- API/Services/Constract/INotificationService.cs
+++ API/Services/Constract/INotificationService.cs
@@ -1,9 +1,11 @@
-﻿using FirebaseAdmin.Messaging;
+﻿using API.Models.DTO;
+using FirebaseAdmin.Messaging;
 
 namespace API.Services.Constract
 {
     public interface INotificationService
     {
         Task<string> SendPushNotification(Message message);
+        Task<Domain.Entities.Notification> PushNotification(NotificationDTO dto, bool save = true);
     }
 }
