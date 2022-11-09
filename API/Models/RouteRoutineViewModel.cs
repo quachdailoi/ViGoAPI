80affb32ddf74bdb695d7404498a3e60497bbc0d
quachdailoi <loiqd.work@gmail.com>
2022-11-09 10:16:28 +0700

    [LoiQD] Fix response of Get RouteRoutine API.

--- API/Models/RouteRoutineViewModel.cs
+++ API/Models/RouteRoutineViewModel.cs
@@ -6,6 +6,7 @@ namespace API.Models
     public class RouteRoutineViewModel
     {
         public string RouteCode { get; set; }
+        public double Distance { get; set; }
         public string StartAt { get; set; }
         public string EndAt { get; set; }
         public string StartTime { get; set; }
