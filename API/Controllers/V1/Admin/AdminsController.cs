b7774a1e9cf2b6d7fbf6ecb9e213847be175a24b
DESKTOP-4BHH352\Win <datdt.dev.work@gmail.com>
2022-11-19 03:46:07 +0700

    [Validator]: fix update setting request validator

--- API/Controllers/V1/Admin/AdminsController.cs
+++ API/Controllers/V1/Admin/AdminsController.cs
@@ -94,8 +94,8 @@ namespace API.Controllers.V1.Admin
         /// Sample request:
         ///     GET api/admins/dashboard
         ///     {
-        ///         "FromDate": "01/01/2022",
-        ///         "ToDate": "05/05/2022"
+        ///         "FromMonth": "05/2022",
+        ///         "ToMonth": "11/2022"
         ///     }
         /// ```
         /// </remarks>
