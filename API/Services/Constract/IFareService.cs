815773e18e56f34b8f00daa8dd2af070a76bba9f
DESKTOP-4BHH352\Win <datdt.dev.work@gmail.com>
2022-11-21 11:36:20 +0700

    [Fix]: fix sharing refund

--- API/Services/Constract/IFareService.cs
+++ API/Services/Constract/IFareService.cs
@@ -5,7 +5,7 @@ namespace API.Services.Constract
 {
     public interface IFareService
     {
-        Task<FeeViewModel> CaculateFeeByDistance(VehicleTypes.SpecificType vehicleTypeId, double distance, TimeOnly time, bool includeBasePrice = true);
+        Task<FeeViewModel> CaculateFeeByDistance(VehicleTypes.SpecificType vehicleTypeId, double distance, TimeOnly time, bool includeBasePrice = true, bool roundToThousands = true);
         Task<FeeViewModel> CaculateBookingFee(Bookings.Types bookingType, VehicleTypes.SpecificType vehicleTypeId, DateOnly startDate, DateOnly endDate, double distance, TimeOnly time, double discount = 0);
         Task<FeeViewModel> CaculateBookingFee(VehicleTypes.SpecificType vehicleTypeId, DateOnly startDate, DateOnly endDate, List<DayOfWeek> dayOfWeeks, double distance, TimeOnly time, double discount = 0);
         Task<FeeViewModel> CaculateFeePerTrip(VehicleTypes.SpecificType vehicleTypeId, DateOnly startDate, DateOnly endDate, List<DayOfWeek> dayOfWeeks, double distance, TimeOnly time);
