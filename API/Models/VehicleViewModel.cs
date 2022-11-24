718844141ae4915f6b4307c65280742f2784eef9
quachdailoi <loiqd.work@gmail.com>
2022-11-24 23:36:16 +0700

    [LoiQD] Fix api get driver by code.

--- API/Models/VehicleViewModel.cs
+++ API/Models/VehicleViewModel.cs
@@ -2,9 +2,11 @@
 {
     public class VehicleViewModel
     {
+        public int Id { get; set; }
         public Guid Code { get; set; }
         public string Name { get; set; }
         public string LicensePlate { get; set; }
         public string Type { get; set; }
+        public int VehicleTypeId { get; set; }
     }
 }
